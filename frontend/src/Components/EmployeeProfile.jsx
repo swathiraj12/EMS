a5eca200b20148120b8b183840c2d25c9d900303
swathiraj12 <swathijcvl@gmail.com>
2024-10-07 13:01:13 +0530

    changing card

--- frontend/src/Components/EmployeeProfile.jsx
+++ frontend/src/Components/EmployeeProfile.jsx
@@ -3,6 +3,7 @@ import '../Assets/CSS/ViewEmployee.css'
 import axios from 'axios'
 import { useAuth } from '../Context/ContextAuth'
 import PreLoader from './PreLoader'
+import cover from '../Assets/Images/dashboardBanner.png'
 
 const EmployeeProfile = () => {
     const { user } = useAuth()
@@ -58,6 +59,42 @@ const EmployeeProfile = () => {
                                         <br /> {employee.address?.address2},<br /> {employee.address?.state},<br /> {employee.address?.pincode}</h6>
                                     <h6 className="mt-3"><span>Date of Join:</span> {employee.doj}</h6>
                                     <h6 className="mt-3"><span>Salary:</span> {employee.salary}</h6>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="card">
+                            <div className="card-body">
+                                <div style={{ position: 'relative', height: '150px', overflow: 'visible' }}>
+                                    <img src={cover} alt="Cover" className="img-fluid" style={{ height: "200px", width: "100%" }} />
+                                    <img src={employee.picture?.imageUrl} alt="Profile" className="img-fluid rounded-circle" style={{ height: "100px", width: "100px", position: "absolute", bottom: "-85px", left: "50%", transform: "translateX(-50%)" }} />
+                                </div>
+                                <div className="row mt-5">
+                                    <h2 className="text-center fw-semibold  mt-5">Sri Ram R <span className="text-secondary fs-6">(EMP)</span></h2>
+                                    <h5 className="text-center text-secondary">(Developer)</h5>
+                                </div>
+                                
+                                <div className="row mt-4">
+                                    <div className="col-lg-6">
+                                        <label htmlFor="" className="form-label fw-bold">Address :</label>
+                                        <h5 className="fw-normal">Door no, street, colony, district, state, country code</h5>
+                                        <label htmlFor="" className="form-label fw-bold mt-3">Phone Number :</label>
+                                        <h5 className="fw-normal">9876543210</h5>
+                                    </div>
+                                    <div className="col-lg-6">
+                                        <label htmlFor="" className="form-label fw-bold">Email :</label>
+                                        <h5 classname="fw-normal">abc@gmail.com</h5>
+                                        <label htmlFor="" className="form-label fw-bold">Blood :</label>
+                                        <h5 className="fw-normal">A2B +</h5>
+                                        <label htmlFor="" className="form-label fw-bold">DOJ :</label>
+                                        <h5 className="fw-normal">01-01-2024</h5>
+                                    </div>
+                                </div>
+                                <div className="d-flex justify-content-between">
+                                    <button className="btn btn-primary">Edit</button>
+                                    <button className="btn btn-success">Delete</button>
+                                    <button className="btn btn-warning">Update</button>
+
                                 </div>
                             </div>
                         </div>
